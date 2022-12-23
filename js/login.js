6d956221e9cb875053166a8a4f5a30bffa99b057
PWJ <1985887536@qq.com>
2022-12-23 21:14:00 +0800

    loginchange

--- js/login.js
+++ js/login.js
@@ -1,58 +1,57 @@
 let users=[{name:'user1',pwd:'111111'},{name:'user2',pwd:'222222'},{name:'user3',pwd:'333333'}];
-let admin=[{name:'pwj',pwd:'20184404'},{name:'admin',pwd:'123456'}]
+let admins=[{name:'pwj',pwd:'20184404'},{name:'admin',pwd:'123456'}];
+var i=0;
+var f1=0;
+var f2=0;
+var f11=0;
+var f12=0;
+var f21=0;
+var f22=0;
 
-let username=document.getElementById("uname");
-let pwd=document.getElementById("pwd");
-let btn=document.getElementById("login");
-
-function setCookie(uname){
-    document.cookie = uname;
+function setCookie(cname){
+    document.cookie = cname;
 }
 
 function checkCookie(){
-    let i = 0;
-      for(i = 0; i < users.length;i++){
-        f1 = document.cookie.indexOf(users[i].name);
-        if(f1 >= 0)break;
-      }
-      for(i = 0; i < users.length;i++){
-        f2 = document.cookie.indexOf(users[i].name);
-        if(f2 >= 0)break;
-      }
-      if (f1>=0||f2>=0){
+    for(i=0;i<users.length;i++){
+        f1 = document.cookie.indexOf(users.name);
+        if(f1>=0)break;
+    }
+    for(i=0;i<admins.length;i++){
+        f1 = document.cookie.indexOf(users.name);
+        if(f1>=0)break;
+    }
+    if (f1>=0||f2>=0){
         alert("您已经登录");
-        location.href="index.html";
-      }
+        window.location.href="index.html";
+    }
 }
 
-function check_login(){
-    var username=document.getElementById("uname").value;
-    var passwd=document.getElementById("psw").value;
-    let ret=users.some(function(value){
-        return value.name==username.value && value.pwd==pwd.value;
-    });
-    
-    let check_admin=admin.some(function(value){
-        return value.name==username.value && value.pwd==pwd.value;
-    });
-    
-    if(username.value==localStorage.getItem('username1') && pwd.value==localStorage.getItem('pwd1')){
-        alert("用户登入成功");
-        window.location.href="index.html";
-            /* 判断是否是预设账户 并判断是用户或管理分别跳转 */		
+function checkUser(){
+    var username=document.getElementById("name").value;
+    var password=document.getElementById("psw").value;
+    for(i=0;i<users.length;i++){
+        f11 = username.indexOf(users.name);
+        f12 = password.indexOf(users.pwd);
+        if(f11>=0&&f12>0){
+            f1=0;
+            break;
+        }
     }
-    else if(ret){
-        setCookie(username);
-        alert("用户登入成功");
-        location.href="../index.html"
+    for(i=0;i<users.length;i++){
+        f21 = username.indexOf(users.name);
+        f22 = password.indexOf(users.pwd);
+        if(f21>=0&&f22>0){
+            f2=0;
+            break;
+        }
     }
-    else if(check_admin){
-        setCookie(username);
-        alert("管理员登入成功");
-        location.href="../index.html";
-    }
-    else{/* 输入错误 */
-        alert("输入正确用户名和密码");
+    if(f1||f2>0){
+            setCookie(username);
+            alert("登录成功！");
+            window.location.href="index.html";
+
+    }else{
+        alert("用户或密码输入错误");
     }
 }
-btn.onclick() = check_login();
