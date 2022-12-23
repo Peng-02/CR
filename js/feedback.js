38f96478f4a37f7519510911ca49596b6c8e3e2a
PWJ <1985887536@qq.com>
2022-12-23 22:50:10 +0800

    login

---
+++ js/feedback.js
@@ -0,0 +1,9 @@
+let username=document.getElementById("contact");
+let phonenum=document.getElementById("tel");
+let pwd1=document.getElementById("pwd");
+let pwd2=document.getElementById("pwd_confirm");
+
+    /* 点击按钮后触发 */
+function check(){
+		
+} 
--- js/login.js
+++ js/login.js
@@ -10,11 +10,11 @@ function checkCookie(){
     var f1=-1;
     var f2=-1;
     for(i=0;i<users.length;i++){
-        f1 = document.cookie.indexOf(users.name);
+        f1 = document.cookie.indexOf(users[i].name);
         if(f1>=0)break;
     }
     for(i=0;i<admins.length;i++){
-        f2 = document.cookie.indexOf(users.name);
+        f2 = document.cookie.indexOf(admins[i].name);
         if(f2>=0)break;
     }
     if (f1>=0||f2>=0){
@@ -30,30 +30,30 @@ function checkUser(){
     var f12=-1;
     var f21=-1;
     var f22=-1;
-    var username=document.getElementById("name").value;
+    var username=document.getElementById("uname").value;
     var password=document.getElementById("psw").value;
     for(i=0;i<users.length;i++){
-        f11 = username.indexOf(users.name);
-        f12 = password.indexOf(users.pwd);
-        if(f11>=0&&f12>0){
+        f11 = username.indexOf(users[i].name);
+        f12 = password.indexOf(users[i].pwd);
+        if(f11>=0&&f12>=0){
             f1=0;
             break;
         }
     }
-    for(i=0;i<users.length;i++){
-        f21 = username.indexOf(users.name);
-        f22 = password.indexOf(users.pwd);
-        if(f21>=0&&f22>0){
+    for(i=0;i<admins.length;i++){
+        f21 = username.indexOf(admins[i].name);
+        f22 = password.indexOf(admins[i].pwd);
+        if(f21>=0&&f22>=0){
             f2=0;
             break;
         }
     }
-    if(f1>0){
+    if(f1>=0){
             setCookie(username);
             alert("用户登录成功！");
             window.location.href="index.html";
     }
-    else if(f2>0){
+    else if(f2>=0){
         setCookie(username);
         alert("管理员登录成功！");
         window.location.href="index.html";
